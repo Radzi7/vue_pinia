a39a5509e3b2adf6d1aab93a86a8ec8c4dda30f5
Radzi7 <eaglemegamind2@gmail.com>
2023-03-13 11:12:07 +0500

    vue_pinia

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { count } from '../stores/counter'
+const store = count()
+</script>
+
+<template>
+  <main>
+    <h1>Home view</h1>
+    <h1>{{ store.number }}</h1>
+    <button @click="store.increment()">ADD</button>
+  </main>
+</template>
+
+<style scoped>
+button {
+  padding: 10px;
+  background: greenyellow;
+}
+</style>
